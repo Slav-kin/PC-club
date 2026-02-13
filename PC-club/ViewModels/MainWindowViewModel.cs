5bd06292b63f753b8125cc844c101421a3a7e08b
yaroslavfuryk <yaroslav.furyk@kpk-lp.com.ua>
2026-02-13 21:42:28 +0200

    Add project files.

---
+++ PC-club/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PC_club.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ PC-club/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PC_club.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
